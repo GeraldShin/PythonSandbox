23661a6ef4b2ab5c60b1cf0c0d2894b2688ef03b
Gerald Shin <43006930+GeraldShin@users.noreply.github.com>
2019-12-11 14:50:11 -0500

    Update OnlineCode.py
    adding some snippets found in medium article

--- OnlineCode.py
+++ OnlineCode.py
@@ -21,3 +21,42 @@ def visit(city):
 for city in cities:
     visit(city)
     
+def split_lines(s): #split a string into pieces using a separator
+  return s.split('\n')
+split_lines('50\n python\n snippets')
+
+
+language = "python" #reverse the order of the letters in a word                                 
+reversed_language = language[::-1]
+print(reversed_language)
+
+def union(a,b): #find elements that exist in both lists
+  return list(set(a + b))
+union([1, 2, 3, 4, 5], [6, 2, 8, 1, 4])
+
+
+def unique(list): #finds if all elements in a list are unique
+    if len(list)==len(set(list)):
+        print("All elements are unique")
+    else:
+        print("List has duplicates")
+unique([1,2,3,4,5]) # All elements are unique
+
+
+from collections import Counter #counts freq of appearance of elements
+list = [1, 2, 3, 2, 4, 3, 2, 3]
+count = Counter(list)
+print(count) # {2: 3, 3: 3, 1: 1, 4: 1}
+
+def most_frequent(list): #piggy-backing, finds most freq appearance of elements
+    return max(set(list), key = list.count)
+numbers = [1, 2, 3, 2, 4, 3, 1, 3]
+most_frequent(numbers) # 3
+
+def multiply(n): #mapping applies the function in the parens to the data element in the parens
+    return n * n 
+  
+list = (1, 2, 3) 
+result = map(multiply, list) 
+print(list(result)) # {1, 4, 9}
+
